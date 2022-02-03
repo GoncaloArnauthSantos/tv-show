c77184e676cbdfd46e2c91ced72a29802ffecec5
Goncalo Santos <goncalo.santos@diconium.com>
2022-02-03 16:52:48 +0000

    feat: tv-show initial setup - project setup - add eslint rules

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { initializeStore } from './store/state/store';
+import Home from './components/templates/home';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={initializeStore}>
+    <Home />
+  </Provider>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
